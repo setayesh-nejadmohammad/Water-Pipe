7e4a855eff029dc63ce2a1a141db457565b0f890
Setayesh <nejadmohammad.setayesh@gmail.com>
2025-04-12 22:30:31 +0330

    add files

--- src/main/java/game/waterpipe/HelloApplication.java
+++ src/main/java/game/waterpipe/HelloApplication.java
@@ -3,22 +3,25 @@ package game.waterpipe;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    private int SIZE = 5; // 5x5 grid --> This will change if user pick LEVEVL 2
-    private static final int CELL_SIZE = 70; // Size of each square cell
-    private static final int SCENE_SIZE = 700; // 700x700 scene
+        private int SIZE = 5; // 5x5 grid --> This will change if user pick LEVEVL 2
+        private static final int CELL_SIZE = 70; // Size of each square cell
+        public static final int SCENE_WIDTH = 900;
+        public static final int SCENE_HEIGHT = 700;
+    //Image icon = new Image(getClass().getResourceAsStream("/pics/water-pipe.png"));
 
     @Override
     public void start(Stage stage) throws Exception {
-        Stage stageChooseLevel = new Stage();
-
+        chooseLevel chonenlevel = new chooseLevel();
+        chonenlevel.DrawStage();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
